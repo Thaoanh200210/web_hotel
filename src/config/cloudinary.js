2b3b9a7d94c6e43c7998622f10d899f7894154d2
Thaoanh200210 <nhuthuyloli1010@gmail.com>
2024-09-04 11:19:47 +0700

    update private secured

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -4,7 +4,7 @@ const cloudinary = require("cloudinary").v2;
 // Return "https" URLs by setting secure: true
 cloudinary.config({
   cloud_name: "distlngqc",
-  api_key: "758885667496425",
+  api_key: process.env.CLOUDINARY_KEY,
   api_secret: process.env.CLOUDINARY_KEY_SECRET,
 });
 
